9362f8edb11b6aedbb374f9ba7a5888e4b183b3f
Renad <uniirena@gmail.com>
2025-01-27 14:24:24 +0300

    Initial Commit

---
+++ Bakeey/BakeeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeeyApp.swift
+//  Bakeey
+//
+//  Created by Renad Alyousef on 27/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BakeeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakeeyTests/BakeeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BakeeyTests.swift
+//  BakeeyTests
+//
+//  Created by Renad Alyousef on 27/07/1446 AH.
+//
+
+import Testing
+@testable import Bakeey
+
+struct BakeeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
